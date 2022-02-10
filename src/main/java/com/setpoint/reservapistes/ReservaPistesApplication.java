adaea293dbfb89e9c3a424fa11d91fb9fe0e79cf
marcg2220 <mrcgrciamr@gmail.com>
2022-02-10 16:53:36 +0100

    first commit

---
+++ src/main/java/com/setpoint/reservapistes/ReservaPistesApplication.java
@@ -0,0 +1,12 @@
+package com.setpoint.reservapistes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaPistesApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(ReservaPistesApplication.class, args);
+    }
+}
